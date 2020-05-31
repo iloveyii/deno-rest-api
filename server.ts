31692417dd8ddb6119c7844401c0369619a7d098
Hazrat Ali <iloveyii@yahoo.com>
2020-05-31 16:15:41 +0200

    Added server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,14 @@
-import { serve } from "https://deno.land/std@0.54.0/http/server.ts";
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const port = 5555;
+const app = new Application();
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+router.get("/api/v1/users", ({ response }: { response: any }) => {
+  response.body = "Hello world!";
+});
+
+console.log(`Server listening on port ${port}`);
+await app.listen({ port });
